707031c98e904aa45af845b59f199dae412fbca4
DVYNE <agboolafemi03@gmail.com>
2024-03-08 01:05:40 +0100

    m

--- Makefile
+++ Makefile
@@ -43,3 +43,5 @@ endif
 
 deploy:
 	@forge script script/DeployPasswordStore.s.sol:DeployPasswordStore $(NETWORK_ARGS)
+
+	forge script script/DeployPasswordStore.s.sol:DeployPasswordStore --rpc-url http://localhost:8545 --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 --broadcast
--- src/PasswordStore.sol
+++ src/PasswordStore.sol
@@ -11,6 +11,8 @@ contract PasswordStore {
     error PasswordStore__NotOwner();
 
     address private s_owner;
+    // @audit the s_password variable is not actually private! 
+    //this is not a safe place to secure your password
     string private s_password;
 
     event SetNetPassword();
@@ -23,13 +25,19 @@ contract PasswordStore {
      * @notice This function allows only the owner to set a new password.
      * @param newPassword The new password to set.
      */
-    function setPassword(string memory newPassword) external {
+    //q can a non owner  set password? 
+    //should a non owner set password?
+    // @audit any user can set password
+    //missing access control
+
+    function setPassword(string memory newPassword) external {  
         s_password = newPassword;
         emit SetNetPassword();
     }
 
     /*
      * @notice This allows only the owner to retrieve the password.
+     // @audit there is no new password parameter  
      * @param newPassword The new password to set.
      */
     function getPassword() external view returns (string memory) {
